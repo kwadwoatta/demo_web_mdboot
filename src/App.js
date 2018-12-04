cf94116fc711a9ce575feb6bef0c345f8f4c257e
Prince Ofori <princeofori065@gmail.com>
2018-12-04 14:46:25 +0000

    added logo and some text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from "mdbreact";
+import logo from './logo.png'
 import Cardd from './components/card/Card';
 import Navbar from './components/navbar/navbar';
 import VideoCaro from './components/carousel/videocarousel';
@@ -11,18 +12,19 @@ class App extends Component {
 
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header ">
-      //     <img src={logo} alt="logo" className="App-logo" />
-      //     <h1 className="App-title">Welcome to Your MDB React App</h1>
-      //   </header>
-      //   <p className="mb-2">The application is configured and ready to import our components.</p>
-      //   <Button href="https://mdbootstrap.com/react/" target="blank" color="light-blue"><strong>Check out our docs!</strong></Button>
-      // </div>
+      
 
     <div>
     	<Navbar/>
 	    <VideoCaro/>
+      <div className="App">
+         <header className="App-header ">
+           <img src={logo} alt="logo" className="App-logo" />
+           <h1 className="App-title">Welcome to Your MDB React App</h1>
+         </header>
+         <p className="mb-2">The application is configured and ready to import our components.</p>
+       <button href="https://mdbootstrap.com/react/" target="blank" color="light-blue"><strong>Check out our docs!</strong></button>
+     </div>
       <div>
         <Container>
           <Row>
